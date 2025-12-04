8965f5554d8336c821fe4feb656453a12cb137fe
maksikonfucian-hash <maksi.konfucian@gmail.com>
2025-12-05 00:10:33 +0300

    fix auf3

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 }));
